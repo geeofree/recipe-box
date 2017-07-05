a27468ff225cc76999267d47a8f60994e3ef0d8c
Geoffrey <geo.galagaran@gmail.com>
2017-07-05 22:46:18 +0800

    Write in configuration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader', 'postcss-loader'])
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    compress: true,
+    hot: true,
+    port: 3000
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
