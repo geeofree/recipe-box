30d6067823f2ebb68953bc465986c10b411414cb
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 11:47:02 +0800

    Change initial state to an array; Add 'ADD_RECIPE' and 'GET_RECIPE' case constraints

--- src/reducers/recipe.reducer.js
+++ src/reducers/recipe.reducer.js
@@ -1,5 +1,12 @@
-const RecipeReducer = (state={}, action) => {
-  return state
+const RecipeReducer = (state=[], action) => {
+  switch(action.type) {
+    case 'ADD_RECIPE':
+      return [...state, action.payload]
+    case 'GET_RECIPE':
+      return state.filter(item => item.id === action.payload.id)[0]
+    default:
+      return state
+  }
 }
 
-export default HomeReducer
+export default RecipeReducer
