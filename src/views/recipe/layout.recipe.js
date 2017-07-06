a9a0a487b7b4d76f981a972f0ea38ec574bedc97
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 13:58:05 +0800

    Remake into class component and populate props with addRecipe dispatcher method

--- src/views/recipe/layout.recipe.js
+++ src/views/recipe/layout.recipe.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addRecipe } from '../../actions/recipe.action'
 
-const Recipe = () => (
-  <div className="recipe">
-    <h1>Recipe</h1>
-  </div>
-)
 
+var id = 0
+
+class RecipeLayout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+
+    this.state = {
+      name: '',
+      ingredients: ''
+    }
+  }
+
+  handleChange(type) {
+    switch(type) {
+      case 'input':
+        return ({ target }) => this.setState({ name: target.value })
+      case 'textarea':
+        return ({ target }) => this.setState({ ingredients: target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const { state, props } = this
+    const { name, ingredients } = state
+    const { addRecipe } = props
+
+
+    addRecipe({ id, name, ingredients: ingredients.split(/\s*,\s*/) })
+    id = id + 1
+  }
+
+  render() {
+    const { props, state, handleSubmit, handleChange } = this
+
+    return (
+      <div className="recipe">
+        <form onSubmit={handleSubmit}>
+          <input placeholder="Recipe Name" onChange={handleChange('input')}/>
+          <textarea placeholder="Type in ingredients separated, by, a, comma." onChange={handleChange('textarea')}/>
+          <button>Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  addRecipe: (data) => dispatch(addRecipe(data))
+})
+
+const Recipe = connect(null, mapDispatchToProps)(RecipeLayout)
 export default Recipe
