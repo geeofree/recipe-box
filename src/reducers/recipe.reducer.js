dd27a190ae2b8f37ed863e8e4d9426d662da23d7
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 14:00:31 +0800

    Change state into object; Create helper functions addRecipe, assign, and getRecipe

--- src/reducers/recipe.reducer.js
+++ src/reducers/recipe.reducer.js
@@ -1,9 +1,30 @@
-const RecipeReducer = (state=[], action) => {
+const InitialState = {
+  recipes: [],
+  selected: null
+}
+
+function assign(target, newValue) {
+  return Object.assign(target, {}, newValue)
+}
+
+function addRecipe(state, recipe) {
+  return assign(state, {
+    recipes: [...state.recipes, recipe]
+  })
+}
+
+function getRecipe(state, recipeID) {
+  return assign(state, {
+    selected: state.recipes.filter(recipe => recipe.id === recipeID)[0]
+  })
+}
+
+const RecipeReducer = (state=InitialState, action) => {
   switch(action.type) {
     case 'ADD_RECIPE':
-      return [...state, action.payload]
+      return addRecipe(state, action.payload)
     case 'GET_RECIPE':
-      return state.filter(item => item.id === action.payload.id)[0]
+      return getRecipe(state, action.payload.id)
     default:
       return state
   }
