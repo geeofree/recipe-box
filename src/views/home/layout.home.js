b7b2b8c5970037d4202480df5c7c6dc3efd7970b
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 11:45:34 +0800

    Subscribe component to the app's store; Remake component to render conditionally

--- src/views/home/layout.home.js
+++ src/views/home/layout.home.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { getRecipe } from '../../actions/recipe.action'
 
-const Home = () => (
-  <div className="home">
-    <h1>Home</h1>
-  </div>
-)
+const HomeLayout = ({ recipes }) => {
+  if(recipes.length) {
+    return (
+      <div className="home">
+        <div className="recipe-list">
+        {recipes.map((recipe, index) => (
+          <p key={index} onClick={e => getRecipe(index)}>{recipe}</p>
+        ))}
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="home">
+        <h1>No recipes made as of yet.</h1>
+      </div>
+    )
+  }
+}
 
+const mapStoreToProps = (store) => ({
+  recipes: store.recipes
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getRecipe: (id) => dispatch(getRecipe(id))
+})
+
+
+const Home = connect(mapStoreToProps, mapDispatchToProps)(HomeLayout)
 export default Home
