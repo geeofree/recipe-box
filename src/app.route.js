4afeb5832450bbe39fa6cd1854d021a84f4293bd
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 09:21:18 +0800

    Create Route component

---
+++ src/app.route.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+
+const Route = ({ children, store }) => (
+  <Provider store={ store }>
+    <Router>
+      <div>
+        { children }
+      </div>
+    </Router>
+  </Provider>
+)
+
+export default Routes
