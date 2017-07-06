453d779975dcb28633faa2c586a453b13ecd5eca
Geoffrey <geo.galagaran@gmail.com>
2017-07-06 11:48:43 +0800

    Only use RecipeReducer in the app's state

--- src/app.state.js
+++ src/app.state.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import RecipeReducer from './reducers/home.reducer'
+import RecipeReducer from './reducers/recipe.reducer'
 
 const logger = (store) => (state) => (action) => {
   console.log('Dispatching:', action)
@@ -8,5 +8,5 @@ const logger = (store) => (state) => (action) => {
 
 const reducers   =  combineReducers({ recipes: RecipeReducer })
 const middleware =  applyMiddleware(logger)
-const AppState   = createStore(reducers, {})
+const AppState   = createStore(reducers, {}, middleware)
 export default AppState
