a1155c00ddd9386f3c8e14b3be195c86ce21a432
Geoffrey <geo.galagaran@gmail.com>
2017-07-07 18:42:19 +0800

    Add container div for the recipe's name and ingredients; Use div instead of img tag for the recipe's image

--- src/views/home/recipe-selected.home.js
+++ src/views/home/recipe-selected.home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const SelectedRecipe = ({ recipe }) => (
-  <div>
-    <img src={recipe.imgURL} />
-    <h2>{recipe.name}</h2>
-    <ul>
-      {recipe.ingredients.map((ingredient, index) => (
-        <li key={index}>{ingredient}</li>
-      ))}
-    </ul>
+  <div className="selected-recipe">
+    <div className="recipe-image" style={{ backgroundImage: `url(${recipe.imgURL})` }} />
+
+    <div className="recipe-info">
+      <h2>{recipe.name}</h2>
+      <ul>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
   </div>
 )
 
