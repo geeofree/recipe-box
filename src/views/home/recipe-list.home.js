9afde5c666e842238cfcceefdd9705bb48c25b20
Geoffrey <geo.galagaran@gmail.com>
2017-07-07 18:16:18 +0800

    Initialize RecipeList component; Give prop dispatcher method "getRecipe"

---
+++ src/views/home/recipe-list.home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+import { getRecipe } from '../../actions/recipe.action'
+
+
+const RecipeListLayout = ({ recipes, getRecipe }) => (
+  <div className="recipe-list">
+    {recipes.map((recipe, id) => (
+      <p
+        key={id}
+        className="recipe-item"
+        onClick={e => getRecipe(id)}>
+          {recipe.name}
+      </p>
+    ))}
+  </div>
+)
+
+const mapDispatchToProps = (dispatch) => ({
+  getRecipe: (id) => dispatch(getRecipe(id))
+})
+
+const RecipeList = connect(null, mapDispatchToProps)(RecipeListLayout)
+export default RecipeList
