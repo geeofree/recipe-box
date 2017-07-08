9323ef67291ad5ab10977f0640ae22d785a9f82d
Geoffrey <geo.galagaran@gmail.com>
2017-07-08 15:13:50 +0800

    Modularize Ingredients component.

--- src/views/home/recipe-selected.home.js
+++ src/views/home/recipe-selected.home.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
+const Ingredients = ({ items }) => {
+  const ingredients = items.filter(val => val).map((ingredient, index) => <li key={index}>{ingredient}</li>)
+  return ingredients.length ? <ul>{ingredients}</ul> : <p>This recipe has no ingredients.</p>
+}
+
 const SelectedRecipe = ({ recipe }) => (
   <div className="selected-recipe">
     <div className="recipe-img" style={{ backgroundImage: `url(${recipe.imgURL})` }} />
-
     <div className="recipe-info">
       <h2>{recipe.name}</h2>
-      <ul>
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
+
+      <div className="ingredients">
+        <h3>Ingredients</h3>
+        <Ingredients items={recipe.ingredients} />
+      </div>
     </div>
   </div>
 )
