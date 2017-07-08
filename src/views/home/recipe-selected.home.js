8c007a441f6fab63802e32f73a6adea7b741bbd4
Geoffrey <geo.galagaran@gmail.com>
2017-07-08 17:50:00 +0800

    Restructure component back to a functional component instead of a class

--- src/views/home/recipe-selected.home.js
+++ src/views/home/recipe-selected.home.js
@@ -6,20 +6,12 @@ import RecipeInfo    from './recipe-info.home'
 import RecipeOptions from './recipe-options.home'
 
 
-class SelectedRecipeLayout extends React.Component {
-
-  render() {
-    const { props } = this
-    const { recipe, deleteRecipe } = props
-
-    return (
-      <div className="selected-recipe">
-        <RecipeInfo recipe={recipe} />
-        <RecipeOptions recipeID={recipe.id} deleteRecipe={deleteRecipe} />
-      </div>
-    )
-  }
-}
+const SelectedRecipeLayout = ({ recipe, deleteRecipe }) => (
+  <div className="selected-recipe">
+    <RecipeInfo recipe={recipe} />
+    <RecipeOptions recipeID={recipe.id} deleteRecipe={deleteRecipe} />
+  </div>
+)
 
 
 const mapDispatchToProps = (dispatch) => ({
