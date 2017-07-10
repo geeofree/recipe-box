e57858193fa4633faee6e02d6fb2f269b020269e
Geoffrey <geo.galagaran@gmail.com>
2017-07-10 16:06:39 +0800

    Remove subscription to state store

--- src/views/home/recipe-selected.home.js
+++ src/views/home/recipe-selected.home.js
@@ -1,22 +1,13 @@
-import React            from 'react'
-import { connect }      from 'react-redux'
-import { removeRecipe } from '../../actions/recipe.action'
+import React from 'react'
 
 import RecipeInfo    from './recipe-info.home'
 import RecipeOptions from './recipe-options.home'
 
-
-const SelectedRecipeLayout = ({ recipe, deleteRecipe }) => (
+const SelectedRecipe = ({ recipe }) => (
   <div className="selected-recipe">
     <RecipeInfo recipe={recipe} />
-    <RecipeOptions recipeID={recipe.id} deleteRecipe={deleteRecipe} />
+    <RecipeOptions />
   </div>
 )
 
-
-const mapDispatchToProps = (dispatch) => ({
-  deleteRecipe: (id) => dispatch(removeRecipe(id))
-})
-
-const SelectedRecipe = connect(null, mapDispatchToProps)(SelectedRecipeLayout)
 export default SelectedRecipe
