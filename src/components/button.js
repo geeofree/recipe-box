f0a3fd405467c8a2d9b321ed4bf9966292690e5f
Geoffrey <geo.galagaran@gmail.com>
2017-09-01 14:09:24 +0800

    Update showRecipeInfo props

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import { connect } from 'react-redux'
-import { showRecipeInfo } from '../actions/modal.actions'
+import { showModal } from '../actions/modal.actions'
+import { RECIPE_INFO } from '../types/modal.types'
 
 const RecipeInfoBtn = ({ showRecipeInfo }) => (
 	<button onClick={() => showRecipeInfo()}>Open Modal</button>
 )
 
 const mapDispatchToProps = (dispatch) => ({
-	showRecipeInfo: () => dispatch(showRecipeInfo())
+	showRecipeInfo: () => dispatch(showModal(RECIPE_INFO))
 })
 
 export default connect(null, mapDispatchToProps)(RecipeInfoBtn)
