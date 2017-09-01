48636e80e2805ecd98121088b99928300f01d10a
Geoffrey <geo.galagaran@gmail.com>
2017-09-01 11:14:38 +0800

    Add RECIPE_INFO constant to modalType modal prop attribute

--- src/components/Modal/RecipeInfo/index.js
+++ src/components/Modal/RecipeInfo/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Modal from '../modal.container'
 import './recipe-info.style.sass'
 
+import { RECIPE_INFO } from '../../../types/modal.types'
+
 export default () => (
-	<Modal modalType="recipe-info" headerTitle="Recipe Information">
+	<Modal modalType={RECIPE_INFO} headerTitle="Recipe Information">
 		<h1>Hello World!</h1>
 	</Modal>
 )
