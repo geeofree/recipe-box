7b2f7d0a8b8dff1ebf96202a147bd29035357e11
Geoffrey <geo.galagaran@gmail.com>
2017-09-02 23:18:57 +0800

    Remove ReduxLogger middleware

--- src/app.state.js
+++ src/app.state.js
@@ -3,5 +3,5 @@ import ReduxThunk  from 'redux-thunk'
 import ReduxLogger from 'redux-logger'
 import RootReducer from './reducers/root.reducer'
 
-const Middlewares = applyMiddleware(ReduxThunk, ReduxLogger)
+const Middlewares = applyMiddleware(ReduxThunk)//, ReduxLogger)
 export default createStore(RootReducer, Middlewares)
