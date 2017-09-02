f9721ee3e7fb87893d0d64692942fcc6974bf084
Geoffrey <geo.galagaran@gmail.com>
2017-09-02 08:51:02 +0800

    Connect button to open the recipe creator form modal

--- src/components/RecipeBox/box.header.js
+++ src/components/RecipeBox/box.header.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React       from 'react'
+import { connect } from 'react-redux'
+
+import { NEW_RECIPE } from '../../types/modal.types'
+import { showModal }  from '../../actions/modal.actions'
 
 import Header from '../Commons/Header'
 import Button from '../Commons/Button'
 
-export default ({ title }) => (
+const BoxHeader = ({ title, showRecipeForm }) => (
 	<Header className="box-header">
 		<h2 className="box-title">{title}</h2>
-		<Button text="Add Recipe" className="add-recipe" />
+		<Button
+			text="Add Recipe"
+			className="add-recipe"
+			onClick={showRecipeForm} />
 	</Header>
 )
+
+const mapDispatchToProps = (dispatch) => ({
+	showRecipeForm: () => dispatch(showModal(NEW_RECIPE))
+})
+
+export default connect(null, mapDispatchToProps)(BoxHeader)
