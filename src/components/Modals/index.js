df7cc7345216560278b40bff72a6d81b793cb65b
Geoffrey <geo.galagaran@gmail.com>
2017-09-02 08:52:21 +0800

    Add RecipeFormModal component

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import RecipeInfoModal from './RecipeInfo'
+import RecipeFormModal from './NewRecipe'
 
 export default () => (
 	<div id="modals">
 		<RecipeInfoModal />
+		<RecipeFormModal />
 	</div>
 )
